c959b0dd872f526cee77fa479e1b2d6892b1cf60
marufab <marufalbaki@gmail.com>
2020-09-01 19:04:50 +0600

    switch case examle

---
+++ 01_switch_case/switch_case.js
@@ -0,0 +1,31 @@
+let weekday = 1;
+console.log(weekday);
+
+switch (weekday){
+case 1:
+  console.log("Today is sunday");
+  break;
+case 2:
+  console.log("Today is Monday");
+  break;
+case 3:
+   console.log("Today is Tuesday");
+   break;
+case 4:
+    console.log("Today is Wednesday"); 
+    break;  
+case 5:
+  console.log("Today is Thursday");
+  break;
+case 6:
+  console.log("Today is Friday");
+  break;
+case 7:
+  console.log("Today is sanday");
+  break;
+  default:
+    console.log ("Invalid");  
+
+
+}
+
